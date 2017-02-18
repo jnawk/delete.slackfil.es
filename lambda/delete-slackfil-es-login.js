3bd190136b5794db57eae9c1b9b5047adcfcd92f
Philip Dowie <philip.dowie@apitalent.co.nz>
2017-02-18 16:07:13 +1300

    moved scopes out of env into code

--- lambda/delete-slackfil-es-login.js
+++ lambda/delete-slackfil-es-login.js
@@ -1,9 +1,10 @@
 'use strict';
 
 console.log('Loading function');
+var scopes = 'files:write:user identify team:read files:read';
 
 exports.handler = (event, context, callback) => {
-    var location = 'https://slack.com/oauth/authorize?client_id=' + process.env.CLIENT_ID + '&scope=' + process.env.SCOPES + '&redirect_uri=' + process.env.REDIRECT_URI;
+    var location = 'https://slack.com/oauth/authorize?client_id=' + process.env.CLIENT_ID + '&scope=' + scopes + '&redirect_uri=' + process.env.REDIRECT_URI;
     callback(null, {
         statusCode: 302,
         body: "",
