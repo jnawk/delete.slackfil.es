c3d00da51d6a94826f471eaa79458469bab316eb
Philip Dowie <philip@jnawk.nz>
2018-08-29 14:59:46 +1200

    updated back end /api/team to also inject username (#4)

--- lambda/delete-slackfil-es-get-team.js
+++ lambda/delete-slackfil-es-get-team.js
@@ -1,36 +1,45 @@
 'use strict';
-var crypto = require('crypto');
-var https = require('https');
+const crypto = require('crypto');
+const https = require('https');
 
-var algorithm = 'aes-256-ctr';
+const algorithm = 'aes-256-ctr';
 
 function decrypt(text){
-  var decipher = crypto.createDecipher(algorithm, process.env.CRYPTO_PSK);
-  var dec = decipher.update(text,'hex','utf8');
-  dec += decipher.final('utf8');
-  return dec;
+    const decipher = crypto.createDecipher(algorithm, process.env.CRYPTO_PSK);
+    var dec = decipher.update(text,'hex','utf8');
+    dec += decipher.final('utf8');
+    return dec;
 }
 
 exports.handler = (event, context, callback) => {
     console.log(JSON.stringify(event));
-    var encryptedToken = event.headers.Cookie.replace(/.*token=([^;]+).*/, '$1');
-    var token = decrypt(encryptedToken);
+    const encryptedToken = event.headers.Cookie.replace(/.*token=([^;]+).*/, '$1');
+    const token = decrypt(encryptedToken);
 
-    https.get('https://slack.com/api/team.info?token=' + token, function(result){
-        result.on('data', function(data) {
-            var dataString = data.toString('utf-8');
-            var response = JSON.parse(dataString);
+    https.get('https://slack.com/api/auth.test?token=' + token, function(authResult) {
+        authResult.on('data', function(authData) {
+            var response = JSON.parse(authData.toString('utf-8'));
             if(response.ok) {
-                callback(null, {
-                    statusCode: 200,
-                    body: dataString,
+                const user = response.user;
+                https.get('https://slack.com/api/team.info?token=' + token, function(teamResult) {
+                    teamResult.on('data', function(teamData) {
+                        var response = JSON.parse(teamData.toString('utf-8'));
+                        if(response.ok) {
+                            response.user = user;
+                            callback(null, {
+                                statusCode: 200,
+                                body: JSON.stringify(response),
+                            });
+                        } else {
+                            callback('error');
+                        }
+                    });
+                }).on('error', function(/*err*/){
+                    callback('error');
                 });
             } else {
-                callback('error'); 
+                callback('error');
             }
         });
-    }).on('error', function(err){
-        callback('error'); 
     });
-    
 };
